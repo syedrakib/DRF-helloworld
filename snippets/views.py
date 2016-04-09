13c521dc87647b9ab8fa259444f224c28649105a
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-04-09 23:58:32 +0600

    Implement snippet_list as a functional view

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 class JSONResponse(HttpResponse):
 	"""
@@ -10,7 +14,23 @@ class JSONResponse(HttpResponse):
 		kwargs['content_type'] = 'application/json'
 		super(JSONResponse, self).__init__(content, **kwargs)
 
-# Create your views here.
+@csrf_exempt
+def snippet_list(request):
+	"""
+	List all code snippets, or create a new snippet.
+	"""
+	if request.method == 'GET':
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		return JSONResponse(serializer.data)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JSONResponse(serializer.data, status=201)
+		return JSONResponse(serializer.errors, status=400)
 
 
 
