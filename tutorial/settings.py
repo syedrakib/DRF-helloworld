4c106e1de7f2069de79b4d29665bbc1f8092c273
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-04-09 23:11:34 +0600

    Add DB config

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':    'django.db.backends.mysql',
+        'NAME':      'drf_tutorial',
+        'USER':      'root',
+        'PASSWORD':  'root',
+        'HOST':      '127.0.0.1',
+        'PORT':      '3306',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
