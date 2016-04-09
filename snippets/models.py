c28eee517edafa5afcfafd718c3b412fb5c0cdb1
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-04-10 01:20:43 +0600

    Add highlighted and owner information to snippets model

--- snippets/models.py
+++ snippets/models.py
@@ -13,9 +13,11 @@ class Snippet(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=100, blank=True, default='')
 	code = models.TextField()
+	highlighted = models.TextField()
 	linenos = models.BooleanField(default=False)
 	language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
 	style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+	owner = models.ForeignKey('auth.User', related_name='snippets')
 
 	class Meta:
 		ordering = ('created',)
