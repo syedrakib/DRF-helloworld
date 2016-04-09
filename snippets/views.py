cc1b52c3c25396a72b011f3c23bbb1134f1d8db3
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-04-10 03:04:34 +0600

    Create endpoint for snippet highlight

--- snippets/views.py
+++ snippets/views.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics, permissions
+from rest_framework import renderers
+from rest_framework.response import Response
+
+
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes = (
+		renderers.StaticHTMLRenderer,
+	)
+
+	def get(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
